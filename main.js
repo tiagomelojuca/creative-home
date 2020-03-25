52f7a8b3b0a426ec209421cdef2ad90fd05ed9ee
Tiago Melo Juca <tiagomelojuca@gmail.com>
2020-03-25 13:01:28 -0300

    Page Template done after third day of the workshop

---
+++ main.js
@@ -0,0 +1,5 @@
+function toggleForm() {
+    document.querySelector('#modal').classList.toggle('hide');
+    document.querySelector('#modal').classList.toggle('addScroll');
+    document.querySelector('body').classList.toggle('hideScroll');
+}
