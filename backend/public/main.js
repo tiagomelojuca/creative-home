bc73f541d970fff9b41a2ad7e5d1ac03e2b931bb
Tiago Melo Juca <tiagomelojuca@gmail.com>
2020-03-28 12:22:39 -0300

    Finished backend after workshop's last day

--- backend/public/main.js
+++ backend/public/main.js
@@ -3,3 +3,27 @@ function toggleForm() {
     document.querySelector('#modal').classList.toggle('addScroll');
     document.querySelector('body').classList.toggle('hideScroll');
 }
+
+function checkFields() {
+    const valuesToCheck = [
+        "title",
+        "category",
+        "image",
+        "description",
+        "url",
+    ];
+
+    const isEmpty = valuesToCheck.find(function(value) {
+        if( typeof event.target[value].value === "string" && !event.target[value].value.trim() ) {
+            return true;
+        }
+    });
+
+    console.log(isEmpty);
+
+    if(isEmpty) {
+       event.preventDefault();
+       alert('Please fill all fields below to proceed');
+    }
+
+}
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks')
+const db = require('./db');
 
 const server = express();
 
@@ -8,59 +9,59 @@ nunjucks.configure('views', {
     noCache: true,
 });
 
-const allIdeas = [
-
-    {
-        img: "icon.svg",
-        title: "Title 1",
-        category: "Category 1",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-        url: "http://www.tiagomelojuca.com.br/",
-    },
-
-    {
-        img: "icon.svg",
-        title: "Title 2",
-        category: "Category 2",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-        url: "http://www.tiagomelojuca.com.br/",
-    },
-
-    {
-        img: "icon.svg",
-        title: "Title 3",
-        category: "Category 3",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-        url: "http://www.tiagomelojuca.com.br/",
-    },
-
-    {
-        img: "icon.svg",
-        title: "Title 4",
-        category: "Category 4",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-        url: "http://www.tiagomelojuca.com.br/",
-    },
-
-]
-
 server.use(express.static('public'));
+server.use(express.urlencoded( { extended: true } ) );
 
 server.get('/', function(request, response) {
-    const allIdeasReversed = [...allIdeas].reverse();
-    let latestIdeas = []
-    for(idea of allIdeasReversed) {
-        if(latestIdeas.length < 3) {
-            latestIdeas.push(idea);
-        }
-    }
 
-    return response.render('index.html', {getIdeas: latestIdeas} )
-})
+    db.all('SELECT * FROM ideas', function(err, rows) {
+        if (err) {
+            console.log(err);
+            return response.send('Database error!');
+        }
+        
+        const allIdeasReversed = [...rows].reverse();
+        let latestIdeas = []
+        for(idea of allIdeasReversed) {
+            if(latestIdeas.length < 3) {
+                latestIdeas.push(idea);
+            }
+        }
+    
+        return response.render('index.html', {getIdeas: latestIdeas} )
+    })
+
+});
 
 server.get('/ideas', function(request, response) {
-    const allIdeasReversed = [...allIdeas].reverse();
-    return response.render('ideas.html', {getIdeas: allIdeasReversed} );
-})
+
+    db.all('SELECT * FROM ideas', function(err, rows) {
+        if (err) {
+            console.log(err);
+            return response.send('Database error!');
+        }
+
+        const allIdeasReversed = [...rows].reverse();
+        return response.render('ideas.html', {getIdeas: allIdeasReversed} );
+    })
+});
+
+server.post('/', function(request, response) {
+    const query = "INSERT INTO ideas(image, title, category, description, url) VALUES (?, ?, ?, ?, ?);";
+    db.run(query, [
+        request.body.image,
+        request.body.title,
+        request.body.category,
+        request.body.description,
+        request.body.url,
+    ], function(err) {
+        if (err) {
+            console.log(err);
+            return response.send('Database error!');
+        }
+    });
+
+    return response.redirect('/ideas');
+});
 
 server.listen(8090);
